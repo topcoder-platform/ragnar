b94015fe7be18ac01c90c89efbc81b893df1ae0a
Justin Gasper <jmgasper@gmail.com>
2018-03-27 17:08:46 +1100

    Fix the config

--- config.js
+++ config.js
@@ -18,16 +18,16 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'hjijfvbw859',
   // a string of time span, see https://github.com/zeit/ms
   JWT_EXPIRATION: process.env.JWT_EXPIRATION || '2 days',
-  GITHUB_CLIENT_ID: 'c693c425f89405b4caa3',
-  GITHUB_CLIENT_SECRET: '6ad6f613878d1afb91a388a51c1f36a1b6a2abff',
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   // used as base to construct various URLs
   WEBSITE: process.env.WEBSITE || 'http://ragnarlocal.topcoder.com',
   GITHUB_OWNER_CALLBACK_URL: process.env.GITHUB_OWNER_CALLBACK_URL || '/app/github-owner-login',
   OWNER_USER_LOGIN_SUCCESS_URL: process.env.OWNER_USER_LOGIN_SUCCESS_URL || '/app/github-owner',
   USER_ADDED_TO_TEAM_SUCCESS_URL: process.env.USER_ADDED_TO_TEAM_SUCCESS_URL || '/app/github-members/added',
   GITLAB_API_BASE_URL: process.env.GITLAB_API_BASE_URL || 'https://gitlab.com/api/v4',
-  GITLAB_CLIENT_ID: '541a097899ce0c51ec90f15f23c0c890a7de367038f07b38b63e896b00cd0ef1',
-  GITLAB_CLIENT_SECRET: 'd0bf50d01a38409c01fa475b7df3349e64396888f9703d37ff8e7cb1bef46151',
+  GITLAB_CLIENT_ID: process.env.GITLAB_CLIENT_ID,
+  GITLAB_CLIENT_SECRET: process.env.GITLAB_CLIENT_SECRET,
   GITLAB_OWNER_CALLBACK_URL: process.env.GITLAB_OWNER_CALLBACK_URL || '/app/gitlab-owner-login',
   GITLAB_OWNER_USER_LOGIN_SUCCESS_URL:
     process.env.GITLAB_OWNER_USER_LOGIN_SUCCESS_URL || '/app/gitlab-owner',
